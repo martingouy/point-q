22b498daf3ab49e0d56b3c50d42480c4c0db04f8
martingouy <martingouy@MacBook-de-Martin.local>
2014-08-15 02:14:44 -0700

    Documentation

--- DOCUMENTATION/source/conf.py
+++ DOCUMENTATION/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.youtube',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
