81b097bcf01d933c494a5a69fd6d9f868e9a7dac
Alex <akurzhan@gmail.com>
2014-09-07 18:19:09 -0700

    merge from 'scoogan' branch and clean-up

--- TOOLS/pre_process_db.py
+++ TOOLS/pre_process_db.py
@@ -10,7 +10,7 @@ def main():
     fout=sys.argv[-1]
     output = csv.writer(open(fout, 'w'), delimiter=',',quotechar='"', quoting=csv.QUOTE_MINIMAL)
     #    output=open('processed_db.txt','w')
-    with open(f, 'rb') as file:
+    with open(f, 'r') as file:
         reader = csv.reader(file)
         for row in reader:
             output.writerow([row[i] for i in [0,1,11,24,18,12,13,23,14]])
